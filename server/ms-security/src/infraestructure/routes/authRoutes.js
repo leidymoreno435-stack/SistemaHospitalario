b3f5b8830fefe7fad7326150b084789287919b6f
Leidy Moreno <leidymoreno435@gmail.com>
2026-07-26 16:51:28 -0500

    en teoria funciona el usuario, pero falta probar con los token

--- server/ms-security/src/infraestructure/routes/authRoutes.js
+++ server/ms-security/src/infraestructure/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { login, register } from '../adapter-input/authController.js';
-import { authMiddleware } from '../../../infraestructure/middleware/authMiddleware.js';
+//import { authMiddleware } from '../../../infraestructure/middleware/authMiddleware.js';
 
 const router = Router();
 
 router.post('/login', login);
-router.post('/register', authMiddleware, register);
+router.post('/register', register);
 
 export default router;
